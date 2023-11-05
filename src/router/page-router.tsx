d5aa964616efbace7021edc0af4d23b0ef35737c
BrBrov <epoc@list.ru>
2023-11-05 13:17:57 +0300

    refactor: refactor to lazy load page router

--- src/router/page-router.tsx
+++ src/router/page-router.tsx
@@ -1,12 +1,25 @@
 import { RouteObject } from 'react-router-dom';
-import Header from '../components/header/Header';
-import Main from '../components/main/Main';
 import infoRouter from './info-router';
 
 const pageRoutes: Array<RouteObject> = [
   {
     path: '',
-    element: [<Header key={'header'} />, <Main key={'main'} />],
+    lazy: async () => {
+      const Header = await import('../components/header/Header').then(
+        (module) => module.default
+      );
+      const Main = await import('../components/main/Main').then(
+        (module) => module.default
+      );
+      const component = () => (
+        <>
+          <Header />
+          <Main />
+        </>
+      );
+
+      return { Component: component };
+    },
     children: infoRouter,
   },
 ];
