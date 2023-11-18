82d779d446629c90f995b9cba9a61e40ac63b9de
BrBrov <epoc@list.ru>
2023-11-19 00:21:31 +0300

    refactor: update tests for count cards

--- src/components/main/Main.test.tsx
+++ src/components/main/Main.test.tsx
@@ -25,31 +25,33 @@ describe('Test count list', function () {
     });
   });
 
-  // it('Count cards 12', function () {
-  //   const newContext = new DataContext();
-  //   newContext.setCardsData(fakeDataWithTwelveElems);
-  //
-  //   render(
-  //     <ContextResponseData.Provider value={newContext}>
-  //       <Main />
-  //     </ContextResponseData.Provider>,
-  //     { wrapper: BrowserRouter }
-  //   );
-  //   const child: Array<HTMLElement> = screen.getAllByAltText(/Image of /i);
-  //   expect(child.length).toEqual(12);
-  // });
-  //
-  // it('Count cards 24', function () {
-  //   const newContext = new DataContext();
-  //   newContext.setCardsData(fakeDataWithTwentyFourElems);
-  //
-  //   render(
-  //     <ContextResponseData.Provider value={newContext}>
-  //       <Main />
-  //     </ContextResponseData.Provider>,
-  //     { wrapper: BrowserRouter }
-  //   );
-  //   const child: Array<HTMLElement> = screen.getAllByAltText(/Image of /i);
-  //   expect(child.length).toEqual(24);
-  // });
+  it('Count cards 12', async function () {
+    render(
+      <Provider store={storeApp}>
+        <MemoryRouter initialEntries={['?page=1&items=12']}>
+          <Main />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    await waitFor(() => {
+      const child: Array<HTMLElement> = screen.getAllByAltText(/Image of /i);
+      expect(child.length).toEqual(12);
+    });
+  });
+
+  it('Count cards 24', async function () {
+    render(
+      <Provider store={storeApp}>
+        <MemoryRouter initialEntries={['?page=1&items=24']}>
+          <Main />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    await waitFor(() => {
+      const child: Array<HTMLElement> = screen.getAllByAltText(/Image of /i);
+      expect(child.length).toEqual(24);
+    });
+  });
 });
