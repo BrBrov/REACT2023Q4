8e06e8667255b27c3b7acd30e27dc796f96efc41
BrBrov <epoc@list.ru>
2023-11-26 14:53:31 +0300

    feat: add 404 page

--- src/components/page-notfound/PageNotFound.tsx
+++ src/components/page-notfound/PageNotFound.tsx
@@ -1,22 +1,24 @@
 import { ReactNode } from 'react';
 
-import './PageNotFound.scss';
-import {useRouter} from "next/router";
+import styles from './PageNotFound.module.scss';
+import { useRouter } from 'next/router';
 
 function PageNotFound(): ReactNode {
   const router = useRouter();
 
   return (
-    <div className="page__not-found">
-      <span className="page__title-not_found">404...</span>
-      <span className="page__text-not_found">Page not found!</span>
-      <button
-        className="page__button-not_found"
-        type="button"
-        onClick={onRedirect}
-      >
-        <span className="page__button-txt_not_found">Return...</span>
-      </button>
+    <div className={styles.not_found_block}>
+      <div className={styles.page__not_found}>
+        <span className={styles.page__title_not_found}>404...</span>
+        <span className={styles.page__text_not_found}>Page not found!</span>
+        <button
+          className={styles.page__button_not_found}
+          type="button"
+          onClick={onRedirect}
+        >
+          <span className={styles.page__button_txt_not_found}>Return...</span>
+        </button>
+      </div>
     </div>
   );
 
---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+import PageNotFound from '@/components/page-notfound/PageNotFound';
+
+function Page404(): ReactNode {
+  return <PageNotFound />;
+}
+
+export default Page404;
